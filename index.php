255898adf2198c5a068875dbdf53712171fe5a1c
MuhammadHilmiSyahrulmubarok <151746074+MuhammadHilmiSyahrulmubarok@users.noreply.github.com>
2023-11-22 19:54:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -23,17 +23,3 @@
 
 
 
-<?php
-include "koneksi.php";
-
-if(isset($_POST['proses'])){
-    mysqli_query($koneksi, "insert into barang set  
-    kode_barang = '$_POST[kode_barang]',
-    nama_barang = '$_POST[nama_barang]',
-    harga_barang = '$_POST[harga_barang]'");
-
-echo "Data barang baru telah tersimpan";
-
-}
-
-?>
